a1ce5ce8c92c34c331e498d3b5699bf4c1723269
Carlo Castoldi <carlo.castoldi@outlook.com>
2023-02-22 11:59:30 +0100

    rename pls_helper.py -> pls.py file

--- BraiAn/__init__.py
+++ BraiAn/__init__.py
@@ -1,5 +1,5 @@
 from .brain_hierarchy import AllenBrainHierarchy
-from .pls_helpers import PLS
+from .pls import PLS
 from .brain_slice import BrainSlice, merge_slice_hemispheres
 from .sliced_brain import SlicedBrain, merge_sliced_hemispheres
 from .animal_brain import AnimalBrain, merge_hemispheres, filter_selected_regions
