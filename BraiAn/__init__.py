537ccdc5e697a436af0d99e1c8d1269dca755c20
Carlo Castoldi <carlo.castoldi@in.cnr.it>
2023-04-14 10:12:31 +0200

    two groups can now be exported to be analysed in Prism

--- BraiAn/__init__.py
+++ BraiAn/__init__.py
@@ -4,6 +4,7 @@ from .brain_slice import BrainSlice, merge_slice_hemispheres
 from .sliced_brain import SlicedBrain, merge_sliced_hemispheres
 from .animal_brain import AnimalBrain
 from .animal_group import AnimalGroup
+from .statistics import as_prism_data
 from .utils import save_csv, regions_to_plot
 from .plot import plot_groups, plot_cv_above_threshold, plot_region_density, plot_permutation, plot_cross_correlation, plot_salient_regions
 from .plot_chord import draw_chord_plot
---
+++ BraiAn/statistics.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from .brain_hierarchy import AllenBrainHierarchy
+from .animal_group import AnimalGroup
+
+def as_prism_data(normalization, group1: AnimalGroup, group2: AnimalGroup, AllenBrain: AllenBrainHierarchy):
+    groups = [group.name for group in (group1, group2) for _ in group.get_animals()]
+    animals = [animal for group in (group1, group2) for animal in group.get_animals()]
+    df = pd.DataFrame(columns=pd.MultiIndex.from_arrays([groups, animals]))
+    for group in (group1, group2):
+        for animal in group.data.index.unique(1):
+            df[group.name, animal] = group.data.xs(animal, axis=0, level=1, drop_level=True)[group.marker, normalization]
+    major_divisions = AllenBrain.get_areas_major_division(*df.index)
+    df["major_divisions"] = [major_divisions[region] for region in df.index]
+    df.set_index("major_divisions", append=True, inplace=True)
+    return df
