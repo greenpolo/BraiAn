f42183d41d5466af13d70261e4719dd867e702e2
Carlo Castoldi <carlo.castoldi@in.cnr.it>
2023-04-21 15:24:15 +0200

    now you can further configure barplots

--- BraiAn/plot.py
+++ BraiAn/plot.py
@@ -13,19 +13,22 @@ from .brain_hierarchy import AllenBrainHierarchy, MAJOR_DIVISIONS
 
 def plot_animal_group(fig: go.Figure, group: AnimalGroup, normalization: str,
                         AllenBrain: AllenBrainHierarchy, selected_regions: list[str],
-                        color: str, y_offset, use_acronyms=True) -> None:
+                        animal_size: int, color: str, y_offset, use_acronyms=True) -> None:
     avg = group.group_by_region(method=normalization).mean(numeric_only=True)
     sem = group.group_by_region(method=normalization).sem(numeric_only=True)
     y_axis, acronyms = pd.factorize(group.data.loc[selected_regions].index.get_level_values(0))
+    full_names = [AllenBrain.full_name[acronym] for acronym in acronyms]
     if not use_acronyms:
-        ticklabels = [f"{AllenBrain.full_name[acronym]}" for acronym in acronyms]
+        ticklabels = full_names
     else:
-        ticklabels = [f"{acronym}" for acronym in acronyms]
+        ticklabels = acronyms
     
     # Barplot (group)
     fig.add_trace(go.Bar(
                         x = avg.loc[selected_regions],
                         name = f"{group.name} mean",
+                        customdata = np.stack((full_names, sem.loc[selected_regions]), axis=-1),
+                        hovertemplate = normalization+" mean: %{x:2f}±%{customdata[1]:2f} "+group.get_units(normalization)+"<br>Region: %{customdata[0]}<br>Group: "+group.name,
                         marker_color=color,
                         error_x = dict(
                             type="data",
@@ -35,19 +38,20 @@ def plot_animal_group(fig: go.Figure, group: AnimalGroup, normalization: str,
     )
     # Scatterplot (animals)
     regions_data = group.select(selected_regions)
+    animal_regions = [AllenBrain.full_name[acronym] for acronym in regions_data.index.get_level_values(0)]
     animal_names = regions_data.index.get_level_values(1)
     fig.add_trace(go.Scatter(
                         mode = "markers",
                         y = y_axis + y_offset,
                         x = regions_data[normalization],
                         name = f"{group.name} animals",
-                        customdata = np.stack((animal_names, regions_data["area"]), axis=-1),
-                        hovertemplate = "Animal: %{customdata[0]}<br>Area: %{customdata[1]} mm²<br>"+normalization+": %{x:.2f} "+group.get_units(normalization),
+                        customdata = np.stack((animal_regions, animal_names, regions_data["area"]), axis=-1),
+                        hovertemplate = normalization+": %{x:.2f} "+group.get_units(normalization)+"<br>Area: %{customdata[2]} mm²<br>Region: %{customdata[0]}<br>Animal: %{customdata[1]}",
                         opacity=0.5,
                         marker=dict(
                             #color="rgb(0,255,0)",
                             color=color,
-                            size=5,
+                            size=animal_size,
                             line=dict(
                                 color="rgb(0,0,0)",
                                 width=1
@@ -60,7 +64,8 @@ def plot_animal_group(fig: go.Figure, group: AnimalGroup, normalization: str,
 UPPER_REGIONS = ['root', *MAJOR_DIVISIONS]
 
 def plot_groups(normalization: str, AllenBrain: AllenBrainHierarchy, *groups: AnimalGroup,
-                selected_regions: list[str], plot_title="", use_acronyms=True,
+                selected_regions: list[str], plot_title="", title_size=20,
+                axis_size=15, animal_size=5, use_acronyms=True,
                 colors=DEFAULT_PLOTLY_COLORS, width=900,
                 barheight=30, bargap=0.3, bargroupgap=0.0):
     # assumes that selected_regions are sorted in depth-first order of the Allen Brain's ontology
@@ -73,7 +78,7 @@ def plot_groups(normalization: str, AllenBrain: AllenBrainHierarchy, *groups: An
     max_bar_offset = region_axis_width - group_bar_width/2
     y_offsets = nrange(-max_bar_offset, max_bar_offset, n_groups)
     for group, y_offset, color in zip(groups, y_offsets, colors):
-        ticklabels = plot_animal_group(fig, group, normalization, AllenBrain, selected_regions, color, y_offset, use_acronyms=use_acronyms)
+        ticklabels = plot_animal_group(fig, group, normalization, AllenBrain, selected_regions, animal_size, color, y_offset, use_acronyms=use_acronyms)
     
     # Plot major divisions
     active_mjd = tuple(AllenBrain.get_areas_major_division(*selected_regions).values())
@@ -103,14 +108,19 @@ def plot_groups(normalization: str, AllenBrain: AllenBrainHierarchy, *groups: An
 
     # Update layout
     fig.update_layout(
-        title = plot_title,
+        title = dict(
+            text=plot_title,
+            font=dict(size=title_size)
+        ),
         yaxis = dict(
             tickmode="array",
             tickvals=np.arange(0,len(selected_regions)),
-            ticktext=[label+"          " for label in ticklabels]
+            ticktext=[label+"          " for label in ticklabels],
+            tickfont=dict(size=axis_size)
         ),
         xaxis=dict(
             title=groups[0].get_plot_title(normalization),
+            tickfont=dict(size=axis_size),
             rangemode="tozero",
             side="top"
         ),
