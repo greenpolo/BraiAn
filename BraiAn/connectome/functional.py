82b0e1433d606b1cd46f3f97b5ef06d4ddd27532
Carlo Castoldi <carlo.castoldi@in.cnr.it>
2023-05-24 22:32:52 +0200

    playing with connectome/graph analyisis

--- BraiAn/connectome/functional.py
+++ BraiAn/connectome/functional.py
@@ -13,22 +13,26 @@ class FunctionalConnectome:
         else:
             above_threshold = (cc.p <= p_cutoff) & (cc.r >= r_cutoff)
         self.r_cutoff = r_cutoff
-        A = cc.r.copy(deep=True)
-        # zero in weighted matrix[i,j] corresponds to NO edge between i and j
-        A[~above_threshold] = 0
+        if weighted:
+            A = cc.r.copy(deep=True)
+            # zero in weighted matrix[i,j] corresponds to NO edge between i and j
+            A[~above_threshold] = 0
+            new_graph = ig.Graph.Weighted_Adjacency
+        else:
+            A = above_threshold
+            new_graph = ig.Graph.Adjacency
         if not isolated_vertices:
             # remove nodes/regions with no connections
             A = A.loc[above_threshold.any(axis=0), above_threshold.any(axis=0)]
-        if weighted:
-            self.G: ig.Graph = ig.Graph.Weighted_Adjacency(A, mode="lower", loops=False)
-        else:
-            self.G: ig.Graph = ig.Graph.Adjacency(above_threshold, mode="lower", loops=False)
+        self.G: ig.Graph = new_graph(A, mode="lower", loops=False)
         self.__add_vertices_attributes(cc)
         self.__add_edges_attributes(cc)
 
     def __add_vertices_attributes(self, cc: CrossCorrelation):
-        self.G.vs["is_undefined"] = cc.r.isna().all().values
-        self.G.vs["upper_region"] = [cc.upper_regions[v["name"]] for v in self.G.vs]
+        for v in self.G.vs:
+            v_name = v["name"]
+            v["is_undefined"] = cc.r[v_name].isna().all()
+            v["upper_region"] = cc.upper_regions[v_name]
     
     def __add_edges_attributes(self, cc: CrossCorrelation):
         for e in self.G.es:
