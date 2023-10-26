66d26c9820cdf133b1f0e941b19996e0611099fe
Carlo Castoldi <carlo.castoldi@outlook.com>
2023-10-26 19:27:37 +0200

    PLS now supports multiple groups

--- BraiAn/pls.py
+++ BraiAn/pls.py
@@ -9,6 +9,8 @@ Created on Wed Mar  9 22:28:08 2022
 import pandas as pd
 import numpy as np
 import plotly.express as px
+from functools import reduce
+
 from .animal_group import AnimalGroup
 
 class PLS:
@@ -24,33 +26,32 @@ class PLS:
     - Ly (pd dataframe): latent variables of Y, i.e. projection of Y on u.
     '''
     
-    def __init__(self, group_1: AnimalGroup, group_2: AnimalGroup, regions: list[str]) -> None:
-        if len(group_1.markers) > 1 or len(group_2.markers) > 1:
+    def __init__(self, regions: list[str], group_1: AnimalGroup, group_2: AnimalGroup, *groups: AnimalGroup) -> None:
+        groups = [group_1, group_2, *groups]
+        if any(len(g.markers) > 1 for g in groups):
             raise ValueError("PLS of AnimalGroups with multiple markers isn't implemented yet")
-        assert group_1.is_comparable(group_2), "Group 1 and Group 2 are not comparable!\n\
+        assert all(group_1.is_comparable(g) for g in groups[1:]), "Group 1 and Group 2 are not comparable!\n\
 Please check that you're reading two groups that normalized on the same brain regions and on the same marker."
         # Fill a data matrix
-        group_1_animals = group_1.get_animals()
-        group_2_animals = group_2.get_animals()
-        animal_list = list(set(group_1_animals).union(set(group_2_animals)))
+        animal_list = [a for g in groups for a in g.get_animals()]
+        if len(animal_list) != len(set(animal_list)):
+            print("WARNING: some animal(s) are in multiple AnimalGroups")
         animal_list.sort()
         data = pd.DataFrame(index=regions+["group"], columns=animal_list)
 
-        data.loc[regions,group_1.get_animals()] = group_1.select(regions).to_pandas(group_1.markers[0])
-        data.loc["group",group_1.get_animals()] = True
-        data.loc[regions,group_2.get_animals()] = group_2.select(regions).to_pandas(group_2.markers[0])
-        data.loc["group",group_2.get_animals()] = False
+        for group in groups:
+            data.loc[regions,group.get_animals()] = group.select(regions).to_pandas(group.markers[0])
+            data.loc["group",group.get_animals()] = group.name
 
         self.X = data.loc[regions].T.dropna(axis="columns", how="any").astype("float64", copy=False)
-        self.y = data.loc["group"].T.astype("bool", copy=False)
-        self.u, self.s, self.v = self.partial_least_squares_correlation(self.X,self.y)
+        self.Y = pd.get_dummies(data.loc["group"].T)
+        self.u, self.s, self.v = self.partial_least_squares_correlation(self.X,self.Y)
         
         self.Lx = self.X @ self.v
-        self.Ly = pd.get_dummies(self.y) @ self.u
+        self.Ly = self.Y @ self.u
         
-    def partial_least_squares_correlation(self,X,y):
+    def partial_least_squares_correlation(self,X,Y):
     
-        Y = pd.get_dummies(y)
 #        print ("\n X:")
 #        print (X)
 #        print("\n y:")
@@ -58,18 +59,17 @@ Please check that you're reading two groups that normalized on the same brain re
 #        print ("\n gtdummies Y:")
 #        print(Y)
         num_animals,num_groups = Y.shape
-        assert num_groups > 1, "You can't compute the PLS on data coming all from the same group!"
 #        print("\n num_animals:")
 #        print(num_animals)
 #        print("\n num_groups:")
 #        print(num_groups)
 
         # Compute M = diag{1.T * Y}.inv * Y.T * X (the average for each group)
-        M = np.linalg.inv(np.diag(np.ones(num_animals).dot(Y))).dot( Y.T.dot(X) ).astype("float")
+        M = np.linalg.inv(np.diag(np.ones(num_animals) @ Y)) @ (Y.T @ X).astype("float")
 #        print("\n M:")
 #        print(M)
         # Mean-center M to get R
-        R = M - np.ones((num_groups,1)).dot( np.ones((1,num_groups)).dot(M) ) / num_groups
+        R = M - np.ones((num_groups,1)) @ ( np.ones((1,num_groups)) @ M) / num_groups
 #        print("\n other matrix to subtract to m:")
 #        print(num_animals)
 #        print("\n R:")
@@ -83,20 +83,21 @@ Please check that you're reading two groups that normalized on the same brain re
         return u,s,vh.T
     
     def bootstrap_salience_scores(self,rank,num_bootstrap):
-
         u_bootstrap = np.expand_dims(np.zeros(self.u.shape), axis=2).repeat(num_bootstrap,axis=2)
         v_bootstrap = np.expand_dims(np.zeros(self.v.shape), axis=2).repeat(num_bootstrap,axis=2)
 
-        data = pd.concat([self.X,self.y],axis=1)
+        num_animals = self.X.shape[0]
+        Y_np = self.Y.to_numpy()
+        X_np = self.X.to_numpy()
         for i in range(0,num_bootstrap):
             while True:
-                data_sampled = data.sample(n=data.shape[0], replace=True)
-                if data_sampled["group"].any() and (~data_sampled["group"]).any():
-                    # make sure the sampled animals are not all from the same group
+                sample = np.random.randint(0, num_animals, num_animals)
+                Y_sampled = Y_np[sample]
+                if Y_sampled.any(axis=0).all():
+                    # make sure the sample has at least one animal for each group
                     break
-            X_sampled = data_sampled.drop("group",axis=1)
-            y_sampled = data_sampled["group"]
-            u_bootstrap[:,:,i],s,v_bootstrap[:,:,i] = self.partial_least_squares_correlation(X_sampled,y_sampled)
+            X_sampled = X_np[sample]
+            u_bootstrap[:,:,i],s,v_bootstrap[:,:,i] = self.partial_least_squares_correlation(X_sampled,Y_sampled)
 
         v_salience = self.v / v_bootstrap.std(axis=2)
         u_salience = self.u / u_bootstrap.std(axis=2)
@@ -109,28 +110,30 @@ Please check that you're reading two groups that normalized on the same brain re
     def randomly_permute_singular_values(self,num_permutations):
     
         singular_values = np.expand_dims(np.zeros(self.s.shape), axis=0).repeat(num_permutations,axis=0)
+        X_np = self.X.to_numpy()
+        Y_np = self.Y.to_numpy()
         count = 0
         for i in range(num_permutations):
             random_index = np.arange(self.X.shape[0])
             np.random.shuffle(random_index)
             
-            X_perm = pd.DataFrame( self.X.to_numpy().astype("float") )
-            y_perm = pd.Series( self.y.to_numpy()[random_index].astype("float") )
+            #X_perm = X_np[random_index,:]
+            Y_perm = Y_np[random_index,:]
 #            print ("y_perm after i="+ str(i)+": \n")
 #            print (y_perm)
 #            print ("\n x_perm after i="+ str(i)+": \n")
 #            print (X_perm)
             
-            if np.array_equal(y_perm.to_numpy(), self.y.to_numpy()):
+            if np.array_equal(Y_perm, Y_np):
 #                print ("in continue lop after after i="+ str(i)+": \n")
                 continue
 
 #            print("\n for i=" + str(i))
-            u_random, singular_values[count,:], vh_random = self.partial_least_squares_correlation(X_perm,y_perm)
+            u_random, singular_values[count,:], vh_random = self.partial_least_squares_correlation(X_np, Y_perm)
             count += 1
             
         self.singular_values = singular_values[:count,:]
         return self.s,self.singular_values
     
-    def above_threshold(self, threshold):
-        return self.v_salience_scores[(self.v_salience_scores[0] > threshold) | (self.v_salience_scores[0] < -threshold)]
+    def above_threshold(self, threshold, group=0):
+        return self.v_salience_scores[group][self.v_salience_scores[group].abs() > threshold]
