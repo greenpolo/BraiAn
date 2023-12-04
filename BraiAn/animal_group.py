fixed norm_threshold() computation for PLS

BraiAn/animal_group.py
@@ -313,7 +313,6 @@ Please check that you're reading two groups that normalized on the same brain re
         return self.v_salience_scores[group][self.v_salience_scores[group].abs() > threshold]
 
     @staticmethod
-    def norm_threshold(nsigma=3) -> float:
-        assert nsigma >= 1 and nsigma <= 3
-        # returns the μ ± (nsigma)σ of the normal
-        return scipy.stats.norm.ppf([0.6826, 0.9545, 0.9973])[nsigma-1]
+    def norm_threshold(p: float, two_way=True) -> float:
+        assert p > 0 and p < 1
+        return scipy.stats.norm.ppf(1-p/2 if two_way else 1-p)