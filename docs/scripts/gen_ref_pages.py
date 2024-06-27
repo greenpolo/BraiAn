89a1d14dd24a518febe309248b6b8184c488c414
Carlo Castoldi <carlo.castoldi@outlook.com>
2024-06-27 20:19:06 +0200

    API References now use a custom collapsable ToC

--- docs/scripts/gen_ref_pages.py
+++ docs/scripts/gen_ref_pages.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Carlo Castoldi <carlo.castoldi@outlook.com>
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
 """Generate the code reference pages."""
 
 from pathlib import Path
