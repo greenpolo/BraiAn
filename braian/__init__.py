f907031c3c00053321e1d3add1493fc2997e2954
Carlo Castoldi <carlo.castoldi@oulook.com>
2024-11-18 11:58:50 +0100

    v1.0.2 - fix for path resolution in Windows

--- braian/__init__.py
+++ braian/__init__.py
@@ -1,15 +1,17 @@
 import platform
 
-from pathlib import Path
+from pathlib import Path, WindowsPath
 
 match platform.system():
     case "Windows":
         import win32com.client
         shell = win32com.client.Dispatch("WScript.Shell")
-        def resolve_symlink(path: Path):
+        def resolve_symlink(path: str|WindowsPath):
+            if isinstance(path, WindowsPath):
+                return shell.CreateShortCut(path).Targetpath if path.suffix == ".lnk" else path
             return shell.CreateShortCut(path).Targetpath if path.endswith(".lnk") else path
     case _:
-        def resolve_symlink(path):
+        def resolve_symlink(path: Path):
             return path.resolve(strict=True)
 
 from .ontology import *
